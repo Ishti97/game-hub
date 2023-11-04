9b29d48f8998e8182461d42bb1acbd2370ace53d
ishtiak <ishtiakrahman15@gmail.com>
2023-11-05 00:28:44 +0600

    Projet setup and chakra ui install

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default App
